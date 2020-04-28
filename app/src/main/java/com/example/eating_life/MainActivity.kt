913333df38499d60c1a73bb4b174618747226b78
sujlee <sujlee@student.42seoul.kr>
2020-04-28 20:04:48 +0900

    kakao map api test

--- app/src/main/java/com/example/eating_life/MainActivity.kt
+++ app/src/main/java/com/example/eating_life/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.eating_life
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val url = "kakaomap://search?q=맛집&p=37.537229,127.005515";
+
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
     }
 }
