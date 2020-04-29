6e6e58e6b5079f71646643b81ed7a4fcce2445a9
sujlee <sujlee@student.42seoul.kr>
2020-04-29 17:20:44 +0900

    choose random menu. show result using textView.

--- app/src/main/java/com/example/eating_life/MainActivity.kt
+++ app/src/main/java/com/example/eating_life/MainActivity.kt
@@ -3,16 +3,29 @@ package com.example.eating_life
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val url = "kakaomap://search?q=맛집&p=37.537229,127.005515";
+        val menu_array = arrayOf("치킨", "피자", "파스타", "자장면", "짬뽕", "뼈해장국", "순대국밥", "회덮밥")
+        val random = Random()
 
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        button.setOnClickListener {
+            val num = random.nextInt(menu_array.size)
+            val menu = menu_array.get(num)
+            textView.setText(menu)
+        }
+        /*
+        val url = "kakaomap://search?q=맛집&p=37.537229,127.005515"
+
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         startActivity(intent);
+        */
     }
 }
